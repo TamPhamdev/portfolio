bea46bf2c6fec7c3d5a9fa6e58ecf94571777430
= <tampham.dev@gmail.com>
2019-03-27 08:39:42 +0700

    Add menu overlay

--- js/script.js
+++ js/script.js
@@ -3,7 +3,6 @@ const menu = document.querySelector(".menu");
 const menuNav = document.querySelector(".menu-nav");
 const menuBranding = document.querySelector(".menu-branding");
 const navItems = document.querySelectorAll(".nav-item");
-
 let showMenu = false;
 
 menuBtn.addEventListener("click", toggleMenu);
